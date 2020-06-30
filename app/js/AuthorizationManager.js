7156f3b553ea2d62c0e0466812214e89061db1dd
Jakob Ackermann <jakob.ackermann@overleaf.com>
2020-06-30 15:50:59 +0100

    [misc] decaff cleanup: AuthorizationManager: implicit returns

--- app/js/AuthorizationManager.js
+++ app/js/AuthorizationManager.js
@@ -5,14 +5,13 @@
 // Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 let AuthorizationManager
 module.exports = AuthorizationManager = {
   assertClientCanViewProject(client, callback) {
-    return AuthorizationManager._assertClientHasPrivilegeLevel(
+    AuthorizationManager._assertClientHasPrivilegeLevel(
       client,
       ['readOnly', 'readAndWrite', 'owner'],
       callback
@@ -20,7 +19,7 @@ module.exports = AuthorizationManager = {
   },
 
   assertClientCanEditProject(client, callback) {
-    return AuthorizationManager._assertClientHasPrivilegeLevel(
+    AuthorizationManager._assertClientHasPrivilegeLevel(
       client,
       ['readAndWrite', 'owner'],
       callback
@@ -29,57 +28,45 @@ module.exports = AuthorizationManager = {
 
   _assertClientHasPrivilegeLevel(client, allowedLevels, callback) {
     if (allowedLevels.includes(client.ol_context.privilege_level)) {
-      return callback(null)
+      callback(null)
     } else {
-      return callback(new Error('not authorized'))
+      callback(new Error('not authorized'))
     }
   },
 
   assertClientCanViewProjectAndDoc(client, doc_id, callback) {
-    return AuthorizationManager.assertClientCanViewProject(client, function (
-      error
-    ) {
+    AuthorizationManager.assertClientCanViewProject(client, function (error) {
       if (error != null) {
         return callback(error)
       }
-      return AuthorizationManager._assertClientCanAccessDoc(
-        client,
-        doc_id,
-        callback
-      )
+      AuthorizationManager._assertClientCanAccessDoc(client, doc_id, callback)
     })
   },
 
   assertClientCanEditProjectAndDoc(client, doc_id, callback) {
-    return AuthorizationManager.assertClientCanEditProject(client, function (
-      error
-    ) {
+    AuthorizationManager.assertClientCanEditProject(client, function (error) {
       if (error != null) {
         return callback(error)
       }
-      return AuthorizationManager._assertClientCanAccessDoc(
-        client,
-        doc_id,
-        callback
-      )
+      AuthorizationManager._assertClientCanAccessDoc(client, doc_id, callback)
     })
   },
 
   _assertClientCanAccessDoc(client, doc_id, callback) {
     if (client.ol_context[`doc:${doc_id}`] === 'allowed') {
-      return callback(null)
+      callback(null)
     } else {
-      return callback(new Error('not authorized'))
+      callback(new Error('not authorized'))
     }
   },
 
   addAccessToDoc(client, doc_id, callback) {
     client.ol_context[`doc:${doc_id}`] = 'allowed'
-    return callback(null)
+    callback(null)
   },
 
   removeAccessToDoc(client, doc_id, callback) {
     delete client.ol_context[`doc:${doc_id}`]
-    return callback(null)
+    callback(null)
   }
 }
